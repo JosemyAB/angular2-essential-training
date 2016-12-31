0a1bd17afa26d4d18467fed12e2cc3999340ac05
JosemyAB <josemitd@hotmail.com>
2016-12-31 13:04:51 +0100

    Hasta ngFor

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,4 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: 'app/app.component.html',
   styleUrls: ['app/app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  firstMediaItem = {
+    id: 1,
+    name: 'El Hobbit',
+    medium: 'Peliculas',
+    category: 'Fantasy',
+    year: 2013,
+    watchedOn: 1294166565384,
+    isFavorite: false
+  }
+
+  borrarPelicula(mediaItem) {
+    console.log('Borro el item');
+    console.log(mediaItem)
+  }
+}
